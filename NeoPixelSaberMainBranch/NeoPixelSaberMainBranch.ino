4d7260bb671f35413d54700975b9ef242e20ec39
NotQfThisWorld <113789597+NotQfThisWorld@users.noreply.github.com>
2023-03-28 15:14:55 +0200

    Faster Ignition and more!
    Added faster ignition:
     - Code is ugly, but didn't bother figuring out how to do it with loops. Might fix later.
     - Much faster than before.
    
    Removed Shock Sensor:
     - Due to my saber not tolerating hard hits (due to batteries jumping out of place), the shock sensor is now removed.
     - Blaster Effect is now bound to the action button.
     - TipDrag (meltTip before), is now achieved by holding down action while pressing ignition button. Stops when action button is released.
    
    Switched MeltTip with TipDrag:
     - Due to the meltTip effect never looking good, I have decided to go for TipDrag instead.
     - TipDrag currently looks awful. 
    
    Smaller changes:
     - Blast Area more than doubled. (10 previously, 25 now)
     - Removed some unnecessary code.
     - Commented out some debugging code.
     - Slightly changed the color values for yellow (still looks like pee though...)
     - Changed all pins to the pins used on the nano. Have moved away from my "prototyping Uno".

--- NeoPixelSaberMainBranch/NeoPixelSaberMainBranch.ino
+++ NeoPixelSaberMainBranch/NeoPixelSaberMainBranch.ino
@@ -1,12 +1,12 @@
-#include <FastLED.h>
+    #include <FastLED.h>
 
 // Led Strip Pins
-  const int DATA_PINF = 12;      // Front strip PIN // Nano: 10
-  const int DATA_PINB = 13;      // Back strip PIN  // Nano: 11
+  const int DATA_PINF = 10;      // Front strip PIN // Nano: 10
+  const int DATA_PINB = 11;      // Back strip PIN  // Nano: 11
 
 // Number of Leds
-  const int NUM_LEDSF = 144; // Front strip LED_COUNT  // Change to (final led count) on the finished version
-  const int NUM_LEDSB = 144; // Back strip LED_COUNT   // Change to (final led count) on the finished version
+  const int NUM_LEDSF = 118; // Front strip LED_COUNT  // Change to (final led count) on the finished version
+  const int NUM_LEDSB = 118; // Back strip LED_COUNT   // Change to (final led count) on the finished version
 
 // Brigthness
   const int BRIGHTNESS = 50;
@@ -20,10 +20,8 @@ CRGB ledsB[NUM_LEDSB];
 // SENSORS AND BUTTON PINS
   const int igniteButton = 2;  // Nano: 2
   const int actionButton = 4;  // Nano: 4
-  const int shockPin = 5;  // Nano: 9
 
 // State values for different sensor readings (either HIGH or LOW)
-  int shockState;
   int actionBtnState;
   int igniteBtnState;
 
@@ -78,7 +76,7 @@ CRGB ledsB[NUM_LEDSB];
 
 // Sizes
   // How big the blasterdeflect area should be
-    const int blastSize = 10;
+    const int blastSize = 25;
   // Tip size
     const int tipSize = 8; // Should be an even number.
 
@@ -106,9 +104,6 @@ void setup() {
 
   // *** Debugging ***
     Serial.begin(9600);
-  
-  // Defining shockPin as INPUT  
-    pinMode(shockPin, INPUT);
 
   // Defining Led Indicator pins as OUTPUTs
     pinMode(redIndi, OUTPUT);
@@ -134,7 +129,7 @@ void setup() {
 
   setMode(); // Runs the setMode program once, to make sure a color is selected before the user powers up his saber.
 
-  // Turn on Red indicator, to show that the lightsaber is powered on and ready to use
+  // Turn on Red indicator, to show that the lightsaber is fully powered on and ready to use
     digitalWrite(redIndi, 1);
     
 }
@@ -142,19 +137,18 @@ void setup() {
 void loop() {
   
   // State values determined by value comming from buttons/shock sensor
-    shockState = digitalRead(shockPin);
     igniteBtnState = digitalRead(igniteButton);
     actionBtnState = digitalRead(actionButton);
 
   if (bladeOff) { // If blade off...
-    if (igniteBtnState == HIGH && shockState == LOW) { //  Starts the blade if the ignite button is pressed, or if the shocksensor is disturbed (enables stab ignition)
+    if (igniteBtnState == HIGH) { //  Starts the blade if the ignite button is pressed, or if the shocksensor is disturbed (enables stab ignition)
 
       // Start Blade
         startBlade(red, green, blue);
 
 
     }
-    else if (actionBtnState == HIGH) { // Runs the setMode function if the action button is pressed.
+    else if (actionBtnState == HIGH && igniteBtnState == LOW) { // Runs the setMode function if the action button is pressed.
 
       // Cycle to another color
         setMode();
@@ -162,27 +156,27 @@ void loop() {
     }  
   }
   else { // If blade is on...
-    if (igniteBtnState == HIGH && shockState == LOW) { // Retracts the blade if the ignite button is pressed.
+    if (igniteBtnState == HIGH && actionBtnState == LOW) { // Retracts the blade if the ignite button is pressed.
 
       // Retract Blade
         retractBlade();
 
     }
 
-    else if (shockState == LOW && !(igniteBtnState == HIGH) && !(actionBtnState == HIGH)) { // Checks if the knock switch has been disturbed. Runs blasterDeflect function if true.
+    else if (actionBtnState == HIGH && igniteBtnState == LOW && tipChange) { // Checks if the knock switch has been disturbed. Runs blasterDeflect function if true.
 
       // Blaster Deflect Effect
         blasterDeflect();
 
     }
 
-    else if (actionBtnState == HIGH && tipChange && !(shockState == LOW)) { // Checks if actionbutton is pressed, and tipchange is true. Turns on tip if both true
+    else if (actionBtnState == HIGH && tipChange && igniteBtnState == HIGH) { // Checks if actionbutton is pressed, and tipchange is true. Turns on tip if both true
     
       // Melttip on
         meltTip(true);
       
     }
-    else if (actionBtnState == LOW && !tipChange && !(shockState == LOW)) { // Checks if actionbutton is not pressed, and if tipchange is false. Turns of tip if both are false
+    else if (actionBtnState == LOW && !tipChange && igniteBtnState == LOW) { // Checks if actionbutton is not pressed, and if tipchange is false. Turns of tip if both are false
     
       // Melttip off
         meltTip(false);
@@ -200,28 +194,69 @@ void startBlade(int red, int green, int blue) { // Startanimation for blade
   // Turn off red indicator, to turn both leds on (see line 39)
     digitalWrite(redIndi, 0);
 
-  // Turns on the first led first
-    ledsF[0].setRGB( red, green, blue);
-    ledsB[0].setRGB( red, green, blue);
-
-  for(int i=0; i<NUM_LEDSF; i++) { // Starts each pixel with the set color, with DELAYVAL between each
-
-    i++;
+  for(int i=0; i<NUM_LEDSF-9; i++) { // Starts each pixel with the set color, with DELAYVAL between each
 
     ledsF[i].setRGB( red, green, blue);
     ledsB[i].setRGB( red, green, blue);
     ledsF[i + 1].setRGB( red, green, blue);
     ledsB[i + 1].setRGB( red, green, blue);
+    ledsF[i + 2].setRGB( red, green, blue);
+    ledsB[i + 2].setRGB( red, green, blue);
+    ledsF[i + 3].setRGB( red, green, blue);
+    ledsB[i + 3].setRGB( red, green, blue);
+    ledsF[i + 4].setRGB( red, green, blue);
+    ledsB[i + 4].setRGB( red, green, blue);
+    ledsF[i + 5].setRGB( red, green, blue);
+    ledsB[i + 5].setRGB( red, green, blue);
+    ledsF[i + 6].setRGB( red, green, blue);
+    ledsB[i + 6].setRGB( red, green, blue);
+    ledsF[i + 7].setRGB( red, green, blue);
+    ledsB[i + 7].setRGB( red, green, blue);
+    ledsF[i + 8].setRGB( red, green, blue);
+    ledsB[i + 8].setRGB( red, green, blue);
+    ledsF[i + 9].setRGB( red, green, blue);
+    ledsB[i + 9].setRGB( red, green, blue);
     FastLED.show();
+
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
 
     delay(DELAYVAL); // Pause before next pass through loop
     
     pulsingAnimation(); // Play pulsingAnimation, for more vareity
-  }
-  
+    pulsingAnimation();
+    pulsingAnimation(); // Tree times, to make the pulsing as close to the running pulsing as possible
+  } 
+    ledsF[NUM_LEDSF-8].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-8].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-7].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-7].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-6].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-6].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-5].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-5].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-4].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-4].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-3].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-3].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-2].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-2].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF-1].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF-1].setRGB( red, green, blue);
+    ledsF[NUM_LEDSF].setRGB( red, green, blue);
+    ledsB[NUM_LEDSF].setRGB( red, green, blue);
+    FastLED.show();
+
   // Blade is now On
     bladeOff = ! bladeOff; 
-  delay(10);
+    delay(10);
   
   // Turn on green indicator
     digitalWrite(greenIndi, HIGH);
@@ -236,24 +271,68 @@ void retractBlade() { // Retract animation for blade
   // Turn off green indicator, to turn both Leds on (see line 39)
     digitalWrite(greenIndi, 0);
 
-  // Turns off the last led first
-    ledsF[NUM_LEDSF - 1] = CRGB::Black; 
-    ledsB[NUM_LEDSB - 1] = CRGB::Black; 
-      
-  for(int i=NUM_LEDSF; i--;) {  // Turns off all leds, one by one
+  for(int i=NUM_LEDSF-1; i>0; i--) {  // Turns off all leds, one by one
 
-    i--;
-    
     ledsF[i] = CRGB::Black; 
     ledsB[i] = CRGB::Black;
     ledsF[i - 1] = CRGB::Black; 
-    ledsB[i - 1] = CRGB::Black; 
+    ledsB[i - 1] = CRGB::Black;
+    ledsF[i - 2] = CRGB::Black; 
+    ledsB[i - 2] = CRGB::Black; 
+    ledsF[i - 3] = CRGB::Black; 
+    ledsB[i - 3] = CRGB::Black; 
+    ledsF[i - 4] = CRGB::Black; 
+    ledsB[i - 4] = CRGB::Black;
+    ledsF[i - 5] = CRGB::Black; 
+    ledsB[i - 5] = CRGB::Black;
+    ledsF[i - 6] = CRGB::Black; 
+    ledsB[i - 6] = CRGB::Black; 
+    ledsF[i - 7] = CRGB::Black; 
+    ledsB[i - 7] = CRGB::Black; 
+    ledsF[i - 8] = CRGB::Black; 
+    ledsB[i - 8] = CRGB::Black;
+    ledsF[i - 9] = CRGB::Black; 
+    ledsB[i - 9] = CRGB::Black; 
     FastLED.show();
 
+    i--;
+    i--;
+    i--;
+    i--;
+    i--;
+    i--;
+    i--;
+    i--;
+    i--;
+    
     delay(DELAYVAL);
     
     pulsingAnimation(); // Play pulsinganimation, for more vareity
+    pulsingAnimation();
+    pulsingAnimation(); // Tree times, to make the pulsing as close to the running pulsing as possible
   }
+    ledsF[9] = CRGB::Black; 
+    ledsB[9] = CRGB::Black;
+    ledsF[8] = CRGB::Black; 
+    ledsB[8] = CRGB::Black;
+    ledsF[7] = CRGB::Black; 
+    ledsB[7] = CRGB::Black;
+    ledsF[6] = CRGB::Black; 
+    ledsB[6] = CRGB::Black;
+    ledsF[5] = CRGB::Black; 
+    ledsB[5] = CRGB::Black;
+    ledsF[4] = CRGB::Black; 
+    ledsB[4] = CRGB::Black;   
+    ledsF[3] = CRGB::Black; 
+    ledsB[3] = CRGB::Black;
+    ledsF[2] = CRGB::Black; 
+    ledsB[2] = CRGB::Black;
+    ledsF[1] = CRGB::Black; 
+    ledsB[1] = CRGB::Black;
+    ledsF[0] = CRGB::Black; 
+    ledsB[0] = CRGB::Black;
+    FastLED.show();
+
 
   // Blade is now Off
     bladeOff = ! bladeOff;
@@ -279,10 +358,6 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
       pulsingStep = 10; // Sets pulsingStep to 10 (default). Can be changed in switch statement.
       pulseAmount = 2; // Sets pulseAmount to 2 (default). Can be changed in switch statement
     // Color defaults
-      // Secondary color
-        redTip = 200;
-        greenTip = 30;
-        blueTip = 2;
 
   switch (modeCase) {
     case 0: // BLUE
@@ -290,7 +365,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 0;
         green = 0;
         blue = 255;
-
+      // Secondary color
+        redTip = 255;
+        greenTip = 0;
+        blueTip = 100;
       // Third color
         redDef = 200;
         greenDef = 200;
@@ -308,7 +386,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 255;
         green = 0;
         blue = 0;
-
+      // Secondary color
+        redTip = 140;
+        greenTip = 140;
+        blueTip = 200;
       // Third color
         redDef = 255;
         greenDef = 240;
@@ -329,7 +410,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 0;
         green = 255;
         blue = 0;
-
+      // Secondary color
+        redTip = 40;
+        greenTip = 40;
+        blueTip = 255;
       // Third color
         redDef = 200;
         greenDef = 255;
@@ -345,9 +429,12 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
     case 3: // GOLD
       // Primary color
         red = 255;
-        green = 80;
+        green = 90;
         blue = 0;
-
+      // Secondary color
+        redTip = 100;
+        greenTip = 100;
+        blueTip = 200;
       // Third color
         redDef = 255;
         greenDef = 200;
@@ -362,7 +449,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 200;
         green = 10;
         blue = 0;
-
+      // Secondary color
+        redTip = 200;
+        greenTip = 100;
+        blueTip = 100;
       // Third color
         redDef = 255;
         greenDef = 200;
@@ -383,7 +473,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 0;
         green = 200;
         blue = 200;
-
+      // Secondary color
+        redTip = 255;
+        greenTip = 20;
+        blueTip = 100;
       // Third color
         redDef = 240;
         greenDef = 255;
@@ -402,7 +495,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 255;
         green = 0;
         blue = 100;
-
+      // Secondary color
+        redTip = 0;
+        greenTip = 0;
+        blueTip = 255;
       // Third color
         redDef = 240;
         greenDef = 230;
@@ -421,7 +517,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 200;
         green = 0;
         blue = 255;
-
+      // Secondary color
+        redTip = 150;
+        greenTip = 100;
+        blueTip = 100;
       // Third color
         redDef = 255;
         greenDef = 200;
@@ -442,7 +541,10 @@ void setMode() { // Sets Mode (colors, effect intensities etc.)
         red = 190;
         green = 80;
         blue = 80;
-
+      // Secondary color
+        redTip = 255;
+        greenTip = 20;
+        blueTip = 20;
       // Third color
         redDef = 240;
         greenDef = 0;
@@ -502,7 +604,11 @@ void blasterDeflect() { // Blaster Deflect Effect
       
     }
     pulsingAnimation(); // Play pulsinganimation, for more vareity
+    pulsingAnimation();
+    pulsingAnimation(); // Tree times, to make the pulsing as close to the running pulsing as possible  
     FastLED.show();
+
+    delay(5);
   }
 }
 
@@ -518,13 +624,9 @@ void meltTip(bool on) { // Changes the tipleds to the tipmelt color if called wi
     // *** Debugging ***
       Serial.println("MeltTip");
 
-      for(int j=NUM_LEDSB; j>NUM_LEDSB - tipSize/2; j--) { // Turns on the first half of the tipmelt leds in the tipmelt color.
+      for(int j=NUM_LEDSF-1; j>NUM_LEDSF - tipSize; j--) { // Turns on the first half of the tipmelt leds in the tipmelt color.
         ledsF[j].setRGB( tipRed, tipGreen, tipBlue);
         ledsB[j].setRGB( tipRed, tipGreen, tipBlue);
-      }
-      for(int j=NUM_LEDSB-tipSize/2; j>NUM_LEDSB - tipSize; j--) { // Turns on the second half of the tipmelt leds in a slightly brighter color.
-        ledsF[j].setRGB( tipRed - 20, tipGreen - 4, tipBlue + 10);
-        ledsB[j].setRGB( tipRed - 20, tipGreen - 4, tipBlue + 10);
       }
 
     // Tip is now On
@@ -540,7 +642,7 @@ void meltTip(bool on) { // Changes the tipleds to the tipmelt color if called wi
     // *** Debugging ***
       Serial.println("MeltTipStopped");
 
-      for(int j=NUM_LEDSB; j>NUM_LEDSB - tipSize; j--) {
+      for(int j=NUM_LEDSF; j>NUM_LEDSF - tipSize; j--) {
         ledsF[j].setRGB( tipRed, tipGreen, tipBlue);
         ledsB[j].setRGB( tipRed, tipGreen, tipBlue);
       }
@@ -578,7 +680,7 @@ void pulsingAnimation() { // Pulsing animation for variation
     FastLED.show();
   
   // *** Debugging ***
-    Serial.println(pulsingBrightness);
+    //Serial.println(pulsingBrightness);
   
 }
 
